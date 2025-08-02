8d716090db88725181fa04086dd8188f02106ec9
Fabio-Alfredo <00210121@uca.edu.sv>
2025-08-02 14:36:03 -0600

    :sparkles: feat: add new configuration for utils and authorizations

--- ToDoList/ToDoList/Program.cs
+++ ToDoList/ToDoList/Program.cs
@@ -1,29 +1,34 @@
 using Microsoft.EntityFrameworkCore;
 using ToDoList;
+using ToDoList.Configurations;
 using ToDoList.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("Connection");
 builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
+builder.Services.AddJwtAuthentication(builder.Configuration);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 builder.Services.AddRepositories();
 builder.Services.AddServices();
+builder.Services.AddUtils();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
 
+app.UseAuthentication();
+app.UseAuthorization();
 app.UseHttpsRedirection();
 app.MapControllers();
 
