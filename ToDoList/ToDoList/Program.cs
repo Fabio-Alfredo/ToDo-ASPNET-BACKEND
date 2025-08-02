c9d40805e80509dc8f1574d0bb5caff5e20288ca
Fabio-Alfredo <00210121@uca.edu.sv>
2025-08-01 22:07:45 -0600

    :sparkles: feat: usage new services for inyection dependecies

--- ToDoList/ToDoList/Program.cs
+++ ToDoList/ToDoList/Program.cs
@@ -1,15 +1,19 @@
 using Microsoft.EntityFrameworkCore;
+using ToDoList;
 using ToDoList.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("Connection");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-var connectionString = builder.Configuration.GetConnectionString("Connection");
-builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
+builder.Services.AddControllers();
+builder.Services.AddRepositories();
+builder.Services.AddServices();
 
 var app = builder.Build();
 
@@ -21,6 +25,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.MapControllers();
 
 
 app.Run();
